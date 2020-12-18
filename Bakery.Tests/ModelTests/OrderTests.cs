db84fb6b949fba26af02312c9b2cda8fd5ba61f8
HarrisonStrand <harrisonstrand@gmail.com>
2020-12-18 11:53:40 -0800

    add purposeful fail for orderconstructor test

---
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(string), newOrder.GetType());
+    }
+  }
+}
